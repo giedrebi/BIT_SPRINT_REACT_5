6d4faf96e90ae41a4cef0872a6ecdd34565275c5
unknown <giedrebielske@hmail.com>
2022-05-18 11:25:57 +0300

    Changed content and style for Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,39 +1,42 @@
 import React from "react";
 
-function Contact () {
-
-        return (
+function Contact() {
+    return (
         <div
-            className="container"
-            style={{
+        className="container"
+        style={{
             justifyContent: "center",
             alignItems: "center",
             display: "flex",
             marginTop: "50px",
+        }}
+        >
+        <div
+            className="card border-white text-center"
+            style={{
+            backgroundColor: "#219ebc",
+            width: "40%",
+            borderRadius: "20px",
             }}
         >
-            <div
-            className="card border-white text-center"
-            style={{ backgroundColor: "#219ebc", width: "40%" }}
-            >
-            <div
-                className="card-header text-bold p-3 h1"
-                style={{
+            <p
+            className="card-header text-bold p-3 h1"
+            style={{
                 background: "rgba( 255, 255, 255, 0.2 )",
                 boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
                 backdropFilter: "blur( 15px )",
                 border: "1px solid rgba( 255, 255, 255, 0.18 )",
-                }}
+                borderRadius: "10px",
+            }}
             >
-                Contact us
-            </div>
+            Contact us
+            </p>
             <div className="card-text p-2 mt-4 mb-4 h6">
-                You can contact us via phone 88888888888
-            </div>
+            You can contact us via phone +37061234567
             </div>
         </div>
-        );
-    }
-
+        </div>
+    );
+}
 
 export default Contact;
